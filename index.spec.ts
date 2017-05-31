b044d0974869925884ebd7281719724dcb16f0b9
justinlettau <justin.b.lettau@gmail.com>
2017-05-30 20:11:04 -0400

    Add unit tests

---
+++ index.spec.ts
@@ -0,0 +1,73 @@
+import * as util from './index';
+
+describe('ts-util-is methods', () => {
+
+    it('ensure value is an array', () => {
+        expect(util.isArray([])).toEqual(true);
+    });
+
+    it('ensure value is a base64 string', () => {
+        expect(util.isBase64('AgQGCAoMDhASFA==')).toEqual(true);
+    });
+
+    it('ensure value is a boolean', () => {
+        expect(util.isBoolean(true)).toEqual(true);
+    });
+
+    it('ensure value is a date', () => {
+        expect(util.isDate(new Date())).toEqual(true);
+    });
+
+    it('ensure value is a valid date', () => {
+        expect(util.isDateValid(new Date('invalid'))).toEqual(false);
+    });
+
+    it('ensure value is defined', () => {
+        expect(util.isDefined(83)).toEqual(true);
+    });
+
+    it('ensure value is an error', () => {
+        expect(util.isError(new Error())).toEqual(true);
+    });
+
+    it('ensure value is a function', () => {
+        expect(util.isFunction(function () {})).toEqual(true);
+    });
+
+    it('ensure value is a guid', () => {
+        expect(util.isGuid('b8161432-e903-4edc-9429-6eade52e2572')).toEqual(true);
+    });
+
+    it('ensure value is infinity', () => {
+        expect(util.isInfinity(Infinity)).toEqual(true);
+    });
+
+    it('ensure value is null', () => {
+        expect(util.isNull(null)).toEqual(true);
+    });
+
+    it('ensure value is a number', () => {
+        expect(util.isNumber(83)).toEqual(true);
+    });
+
+    it('ensure value is an object', () => {
+        expect(util.isObject({})).toEqual(true);
+    });
+
+    it('ensure value is a regex', () => {
+        expect(util.isRegExp(/83/)).toEqual(true);
+    });
+
+    it('ensure value is a string', () => {
+        expect(util.isString('')).toEqual(true);
+    });
+
+    // it('ensure value is a symbol', () => {
+    //     expect(util.isSymbol(Symbol())).toEqual(true);
+    // });
+
+    it('ensure value is undefined', () => {
+        expect(util.isUndefined(undefined)).toEqual(true);
+    });
+
+});
