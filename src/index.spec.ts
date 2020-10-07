fd82f8b3cbdba6fa66ecb83e320f7ca64452b5e7
Josh Gummersall <1235378+joshgummersall@users.noreply.github.com>
2020-10-07 09:33:42 -0700

    feat: add isNil and isInstance type guards (#20)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -78,4 +78,22 @@ describe('ts-util-is methods', () => {
     expect(util.isUndefined(undefined)).toEqual(true);
   });
 
+  it('ensure value is null or undefined', () => {
+    expect(util.isNil(null)).toEqual(true);
+    expect(util.isNil(undefined)).toEqual(true);
+    expect(util.isNil(false)).toEqual(false);
+    expect(util.isNil(1)).toEqual(false);
+  });
+
+  it('ensure value is an instance of a type', () => {
+    const err = new Error();
+    expect(util.isInstance(err, Error)).toEqual(true);
+
+    const fn = new Function();
+    expect(util.isInstance(fn, Function)).toEqual(true);
+
+    expect(util.isInstance(null, Error)).toEqual(false);
+    expect(util.isInstance(false, Error)).toEqual(false);
+  });
+
 });
